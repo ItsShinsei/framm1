427da0e1707b2a4edb921baa429adddace9dbbb0
ItsShinsei <mnovarahman505@gmail.com>
2025-08-05 11:44:18 +0700

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\homecontroller;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/nova', function () {
+    return view('nova');
 });
+
+route::get('/', [homecontroller::class, 'index'])->name('home');
